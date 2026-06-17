9d216e46ab976908bddd44a874407dea69acd7b6
YanoGoos <kotomi.yano@goos.jp>
2026-06-17 17:32:33 +0900

    ~26/06/17 17:00

--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
-
+/*
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Person;
@@ -7,3 +7,4 @@ import com.example.demo.model.Person;
 public interface  PersonRepository extends JpaRepository<Person,Long>{
 
 }
+*/
